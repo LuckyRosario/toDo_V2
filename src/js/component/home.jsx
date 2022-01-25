a6f3aa0324900f811e808f14cee1f3079a657149
LuckyRosario <p.rosario684@gmail.com>
2022-01-25 12:44:10 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,26 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [todo, setTodo] = useState([
+		"Make the bed",
+		"Brush my teeth",
+		"Make food",
+	]);
+
+	const deleteTodo = (idToDelete) => {
+		setTodos((currentTodos) =>
+			currentTodos.filter((todo) => todo.id !== idToDelete)
+		);
+	};
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center mt-5">
+			<input
+				type="text"
+				onChange={(e) => setTodo(e.target.value)}
+				value={[...todo, currentTodos]}
+			/>
+			<button onClick={deleteTodo} />
 		</div>
 	);
 };
