1fec1a901ea31ec5b6d60071c588a29198b77a0c
LuckyRosario <p.rosario684@gmail.com>
2022-01-25 21:55:49 +0000

    finished functionality

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,28 +1,44 @@
 import React, { useState } from "react";
 
 //create your first component
-const Home = () => {
-	const [todo, setTodo] = useState([
+export default function Home() {
+	const [todos, setTodos] = useState([
 		"Make the bed",
 		"Brush my teeth",
 		"Make food",
 	]);
-
-	const deleteTodo = (idToDelete) => {
-		setTodos((currentTodos) =>
-			currentTodos.filter((todo) => todo.id !== idToDelete)
-		);
+	const addTodo = (e) => {
+		if (e.keyCode == 13) {
+			let task = e.target.value;
+			const newTodos = [...todos, task];
+			setTodos(newTodos);
+			e.target.value = "";
+		}
+	};
+	const deleteTodo = (index) => {
+		const newTodos = [...todos];
+		newTodos.splice(index, 1);
+		setTodos(newTodos);
 	};
 	return (
 		<div className="text-center mt-5">
 			<input
 				type="text"
-				onChange={(e) => setTodo(e.target.value)}
-				value={[...todo, currentTodos]}
+				placeholder={
+					todos.length === 0 ? "No tasks." : "What needs to be done?"
+				}
+				onKeyDown={addTodo}
 			/>
-			<button onClick={deleteTodo} />
+			<ul>
+				{todos.map((todo, index) => (
+					<li className="todo-item" key={index}>
+						{todo}
+						<button className="delete" onClick={deleteTodo}>
+							x
+						</button>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
-};
-
-export default Home;
+}
